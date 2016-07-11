36680e0436aa91e9eed503037f7582f6c11af656
Yi Tang <yi.tang.uk@me.com>
2016-07-11 16:12:29 +0100

    add utility function for
    reading and writing to yaml format

---
+++ optomatic/utils.py
@@ -0,0 +1,12 @@
+import yaml
+
+
+def yload(fpath):
+    with open(fpath, 'r') as f:
+        dat = yaml.load(f)
+    return dat
+
+
+def ywrite(dat, fpath):
+    with open(fpath, 'w') as f:
+        yaml.dump(dat, f, default_flow_style=False)
